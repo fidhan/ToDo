e7edcbdfbb5101f806b01a55e880a3a4c8ac2277
fidhan <fidha.n0103@gmail.com>
2023-05-08 12:41:04 +0530

    added more functions

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTasks/', views.addTasks, name='addTasks'),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+    path('delete_task/<int:pk>', views.delete_task, name='delete_task'),
 
 
 ]
